70b87aa016847d44775ddd3ea1a67d2a656dc4b1
dnmdcplyglt <vishal.federer@gmail.com>
2021-01-10 13:58:46 +0100

    Deleted main.py and README.md

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-pi-example
-This is a example project to demonstrate the use of Git on Ubuntu.
--- main.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/python3
-def main(msg):
-    //No longer need comments here
-    print(msg)
-
-def secondary():
-    print("New function")
-    
-main("Hello People")
